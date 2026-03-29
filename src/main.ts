f21de395c13487e1040805a15dc2950a5e2c8662
emanuelarevalomartinez <emanuelarevalomartinez@gmail.com>
2026-03-29 02:16:50 -0400

    first commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
+
+async function bootstrap() {
+
+  const logger = new Logger(`Orders-Main`);
+
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(envs.port);
+
+  logger.log(`Orders microservice running on port ${envs.port}`);
+
+
+}
+bootstrap();
